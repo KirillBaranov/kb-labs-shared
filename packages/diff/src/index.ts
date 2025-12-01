30600a24cc6f1c604c8806abbc6f0ba3040e6efe
KirillBaranov <kirillBaranovJob@yandex.ru>
2025-12-01 14:00:56 +0300

    refactor(shared): remove diff package
    - Remove diff package and related files

--- packages/diff/src/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./types";
-export { parseUnifiedDiff } from "./parseUnifiedDiff";
-export { listChangedFiles } from "./listChangedFiles";
-export { addedLinesByFile } from "./addedLinesByFile";
-export { removedLinesByFile } from "./removedLinesByFile";
